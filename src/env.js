59e37eb4477e4938bdbd71813829eadf068513c8
GuangHui Ren <rguanghui@users.noreply.github.com>
2016-11-02 17:34:15 +0800

    [Fixed] hybridAPI bug in napos app. (#32)

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const userAgent = window.navigator.userAgent;
 
 export let version;
-if (/rajax|eleme/i.test(userAgent)) {
+if (/eleme/i.test(userAgent)) {
   const matches = userAgent.match(/Eleme\/([0-9]+)\.([0-9]+)/i);
   version = Number(matches[1]) * 100 + Number(matches[2]);
 }

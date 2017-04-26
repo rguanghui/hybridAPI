d48648bee8f4383ae3fcd0d8181396d6d9edbedd
GuangHui Ren <rguanghui@users.noreply.github.com>
2017-04-26 18:28:23 +0800

    + pay scheme (#47)

--- src/apis/pay.js
+++ src/apis/pay.js
@@ -2,17 +2,31 @@ import { invokeMethod } from '../util'
 
 /**
  * @param {Object} params app scheme 参数
- * @param {Number} params.merchant_id 支付的 merchant id
- * @param {Number} params.merchant_order_no 落单后的 order number
- * @param {String=} params.event_name app 回调时发送给 webView 的事件名
+ * @param {Number} params.merchantId 支付的 merchant id
+ * @param {Number} params.merchantOrderNo 落单后的 order number
+ * @param {String=} params.eventName app 回调时发送给 webView 的事件名
  * @param {Function} params.action 接收 app 回调的函数
- * action 可获得的参数：{ merchant_id: 1, merchant_order_no: 'xxx', status: 'success' || 'failed' }
+ * action 可获得的参数：{ merchantId: 1, merchantOrderNo: 'xxx', status: 'success' || 'failed' }
  * @param {String=} params.x_shard 自定义的多活 header
  */
 export default params => {
-  params.event_name = params.event_name || 'NATIVE_PAY_CALLBACk'
-  const { event_name, action } = params
-  document.addEventListener(event_name, action, false)
+  let { eventName, action, merchantId, merchantOrderNo, xShard } = params
+  eventName = eventName || 'NATIVE_PAY_CALLBACk'
 
-  invokeMethod('pay', params)
+  document.addEventListener(eventName, event => {
+    const response = event.detail
+    // App 给我们的格式不是驼峰，这里做下 hard code
+    let callbackParams = {}
+    callbackParams.merchantId = response.merchant_id
+    callbackParams.merchantOrderNo = response.merchant_order_no
+    callbackParams.status = response.status
+    action(callbackParams)
+  }, false)
+
+  let schemeURL = `eleme://pay?merchant_id=${merchantId}&merchant_order_no=${merchantOrderNo}&event_name=${eventName}`
+  if (xShard) {
+    schemeURL += `&x_shard=${xShard}`
+  }
+
+  location.href = schemeURL
 }

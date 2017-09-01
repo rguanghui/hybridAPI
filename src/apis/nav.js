f32cbeb0e687fb930fea20682c87912ce7538be0
YiSiWang <yisi.wang@ele.me>
2017-09-01 14:43:58 +0800

    + add setNav

---
+++ src/apis/nav.js
@@ -0,0 +1,31 @@
+import { invokeMethod } from '../util'
+
+const handleOption = option => {
+  if (typeof option === 'string') {
+    return { solid: option }
+  }
+  return option
+}
+
+export default ({ immersive, statusText, navBg, navText, triggerHeight }) => {
+  if (immersive !== undefined) {
+    invokeMethod('setImmersiveMode', immersive)
+  }
+  if (statusText !== undefined) {
+    if (statusText === 'black') {
+      statusText = { solid: false }
+    } else if (statusText === 'white') {
+      statusText = { solid: true }
+    }
+    invokeMethod('setLightStatusBar', statusText)
+  }
+  if (navBg !== undefined) {
+    invokeMethod('setNavBgColor', handleOption(navBg))
+  }
+  if (navText !== undefined) {
+    invokeMethod('setNavTextColor', handleOption(navText))
+  }
+  if (triggerHeight !== undefined) {
+    invokeMethod('setTriggerHeight', triggerHeight)
+  }
+}
